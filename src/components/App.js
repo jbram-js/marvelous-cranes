b35b6d62e10f51098185fc82dcd0a1894f5745d8
“Joe <josephbramwell@bolstersystems.com>
2020-09-12 11:10:34 +0100

    under coonstruction set up

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../styles/App.css";
+import logo from "../images/logo.svg";
+import crane from "../images/crane.gif";
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} alt="Logo" className="logo-home" />
+      <h1 className="app-header">
+        Welcome to the Cranger Zone! <br></br>🏗️ Under construction 🏗️
+      </h1>
+      <img src={crane} alt="gif" className="crane-gif" />
+    </div>
+  );
+}
+
+export default App;
