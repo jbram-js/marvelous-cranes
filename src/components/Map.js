897db3e796812fccffce94db3540afb897d03d74
“Joe <josephbramwell@bolstersystems.com>
2020-09-19 16:50:21 +0100

    added map to map js

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,11 +1,124 @@
-import React from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import { GoogleMap, Marker, InfoWindow } from "@react-google-maps/api";
+
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+import "@reach/combobox/styles.css";
 import NavBar from "./NavBar";
 
+import "../styles/Map.css";
+
+const mapContainerStyle = {
+  width: "100vw",
+  height: "90vh",
+};
+const center = {
+  lat: 53.480759,
+  lng: -2.242631,
+};
+
 const Map = () => {
+  const mapRef = useRef();
+  const onMapLoad = useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
+  const panTo = useCallback(({ lat, lng }) => {
+    mapRef.current.panTo({ lat, lng });
+    mapRef.current.setZoom(14);
+  }, []);
+
   return (
-    <div className="Profile">
-      <h1>Map</h1>
+    <div className="Map">
+      <Search panTo={panTo} />
+      <Locate panTo={panTo} />
       <NavBar />
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={13}
+        center={center}
+        onLoad={onMapLoad}
+      ></GoogleMap>
+    </div>
+  );
+};
+
+const Locate = ({ panTo }) => {
+  return (
+    <button
+      className="locate"
+      onClick={() => {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            panTo({
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            });
+          },
+          () => null
+        );
+      }}
+    >
+      <img src="compass.svg" alt="find users current location" />
+    </button>
+  );
+};
+
+const Search = ({ panTo }) => {
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete({
+    requestOptions: {
+      location: { lat: () => 53.480759, lng: () => -2.242631 },
+      radius: 200 * 1000,
+    },
+  });
+
+  return (
+    <div className="search">
+      <Combobox
+        onSelect={async (address) => {
+          setValue(address, false);
+          clearSuggestions();
+          try {
+            const results = await getGeocode({ address });
+            const { lat, lng } = await getLatLng(results[0]);
+            panTo({ lat, lng });
+          } catch (error) {
+            console.log("Error!");
+          }
+        }}
+      >
+        <ComboboxInput
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+          disabled={!ready}
+          placeholder="Search a place"
+        />
+        <ComboboxPopover>
+          <ComboboxList>
+            {status === "OK" &&
+              data.map(({ id, description }) => (
+                <ComboboxOption key={id} value={description} />
+              ))}
+          </ComboboxList>
+        </ComboboxPopover>
+      </Combobox>
     </div>
   );
 };
