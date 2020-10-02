b3979356b23416c27ce9dabc77b8baf02058dc85
Nick <nickmarron@hotmail.co.uk>
2020-10-02 14:35:25 +0100

    updated profile to include images and only users cranes

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -15,11 +15,10 @@ const Profile = ({ userId, userLocation }) => {
 
   useEffect(() => {
     axios
-      .get("https://test-crane.herokuapp.com/craneUser", {
-        params: { userID: userId },
-      })
+    .get(`https://test-crane.herokuapp.com/craneUser?userID=${userId}`)
       .then(({ data }) => {
         setAllUsersCranes(data);
+        console.log(data)
         axios
           .get(`https://test-crane.herokuapp.com/${data[0].userID}/users`)
           .then(({ data }) => {
@@ -45,7 +44,7 @@ const Profile = ({ userId, userLocation }) => {
               username={username}
               numberOfLikes={cranes.craneLikes}
               userLocation={userLocation}
-              image={placeholder}
+              image={cranes.image}
             />
           </div>
         ))}
